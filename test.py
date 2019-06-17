c0478bc7b23f9f201579f710f8c1fc0fc4601b4a
Satria Bagus <satria_bagus@hotmail.com>
2019-06-17 16:32:50 +0700

    init project:

---
+++ test.py
@@ -0,0 +1,123 @@
+'''
+    This code is taken from https://github.com/brucechou1983/CheXNet-Keras
+'''
+import numpy as np
+import os
+from configparser import ConfigParser
+from generator import AugmentedImageSequence
+from models.keras import ModelFactory
+from sklearn.metrics import roc_auc_score, accuracy_score, precision_score, recall_score
+from utility import get_sample_counts
+
+
+def main():
+    # parser config
+    config_file = "./config.ini"
+    cp = ConfigParser()
+    cp.read(config_file)
+
+    # default config
+    output_dir = cp["DEFAULT"].get("output_dir")
+
+    base_model_name = cp["DEFAULT"].get("base_model_name")
+    class_names = cp["DEFAULT"].get("class_names").split(",")
+    image_source_dir = cp["DEFAULT"].get("image_source_dir")
+
+    # train config
+    image_dimension = cp["TRAIN"].getint("image_dimension")
+
+    # test config
+    batch_size = cp["TEST"].getint("batch_size")
+    test_steps = cp["TEST"].get("test_steps")
+    use_best_weights = cp["TEST"].getboolean("use_best_weights")
+
+    # parse weights file path
+    output_weights_name = cp["TRAIN"].get("output_weights_name")
+    weights_path = os.path.join(output_dir, output_weights_name)
+    best_weights_path = os.path.join(output_dir, f"best_{output_weights_name}")
+
+    # get test sample count
+    test_counts, _ = get_sample_counts(output_dir, "test", class_names)
+
+    # compute steps
+    if test_steps == "auto":
+        test_steps = int(test_counts / batch_size)
+    else:
+        try:
+            test_steps = int(test_steps)
+        except ValueError:
+            raise ValueError(f"""
+                test_steps: {test_steps} is invalid,
+                please use 'auto' or integer.
+                """)
+    print(f"** test_steps: {test_steps} **")
+
+    print("** load model **")
+    if use_best_weights:
+        print("** use best weights **")
+        model_weights_path = best_weights_path
+    else:
+        print("** use last weights **")
+        model_weights_path = weights_path
+    model_factory = ModelFactory()
+    model = model_factory.get_model(
+        class_names,
+        model_name=base_model_name,
+        use_base_weights=False,
+        weights_path=model_weights_path)
+
+    print("** load test generator **")
+    test_sequence = AugmentedImageSequence(
+        dataset_csv_file=os.path.join(output_dir, "test.csv"),
+        class_names=class_names,
+        source_image_dir=image_source_dir,
+        batch_size=batch_size,
+        target_size=(image_dimension, image_dimension),
+        augmenter=None,
+        steps=test_steps,
+        shuffle_on_epoch_end=False,
+    )
+
+    print("** make prediction **")
+    y_hat = model.predict_generator(test_sequence, verbose=1)
+    y = test_sequence.get_y_true()
+
+    test_log_path = os.path.join(output_dir, "test.log")
+    print(f"** write log to {test_log_path} **")
+    aurocs = []
+    precision = []
+    accuracy = []
+    recall = []
+    with open(test_log_path, "w") as f:
+        for i in range(len(class_names)):
+            try:
+                auroc_score = roc_auc_score(y[:, i], y_hat[:, i])
+                accuracy_score = accuracy_score(y[:, i], y_hat[:, i])
+                precision_score = precision_score(y[:, i], y_hat[:, i])
+                recall_score = recall_score(y[:, i], y_hat[:, i])
+                aurocs.append(auroc_score)
+                precision.append(precision_score)
+                accuracy.append(accuracy_score)
+                recall.append(recall_score)
+            except ValueError:
+                auroc_score = recall_score = accuracy_score = precision_score = 0
+            f.write(
+                f"{class_names[i]}\naccuracy: {accuracy_score}\nprecision: {precision_score}\nrecall: {recall_score}\nauroc: {auroc_score}\n"
+                    )
+        mean_auroc = np.mean(aurocs)
+        mean_accuracy = np.mean(accuracy)
+        mean_precision = np.mean(precision)
+        mean_recall = np.mean(recall)
+        f.write("-------------------------\n")
+        f.write(f"mean accuracy: {mean_accuracy}\n")
+        f.write(f"mean precision: {mean_precision}\n")
+        f.write(f"mean recall: {mean_recall}\n")
+        f.write(f"mean auroc: {mean_auroc}\n")
+        print(f"mean accuracy: {mean_accuracy}\n")
+        print(f"mean precision: {mean_precision}\n")
+        print(f"mean recall: {mean_recall}\n")
+        print(f"mean auroc: {mean_auroc}")
+
+
+if __name__ == "__main__":
+    main()
